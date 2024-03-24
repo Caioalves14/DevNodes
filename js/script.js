db529191e8f1e84fd9be55990c5f30c61af877bc
Caioalves14 <caio.alvesrex@gmail.com>
2024-03-24 17:10:42 -0300

    criando a função de buscar a lista

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const noteInput = document.querySelector("#note-content");
 
 const addNoteBtn = document.querySelector(".add-note");
 
+const searchInput = document.querySelector("#search-input");
+
 // Funções
 
 function showNotes() {
@@ -163,8 +165,42 @@ function getNotes() {
 function saveNotes(notes) {
     localStorage.setItem("notes", JSON.stringify(notes))
 }
+
+function searchNodes(search) {
+    const searchResults = getNotes().filter((note) => {
+       return note.content.includes(search)
+    })
+
+    if(search !== ""){
+        cleanNotes()
+
+        searchResults.forEach((note) => {
+            const noteElement = createNote(note.id, note.content)
+
+            notesContainer.appendChild(noteElement);
+        });
+
+        return;
+    }
+
+    cleanNotes()
+
+    showNotes();
+}
 // Eventos
 addNoteBtn.addEventListener("click", () => addNote());
+
+searchInput.addEventListener("keyup", (e) => {
+    const search = e.target.value 
+
+    searchNodes(search);
+})
+
+noteInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter") {
+        addNote();
+    }
+})
 
 // Inicialização
 showNotes();
